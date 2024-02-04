1e04680cf0e7054b0fd50d3a228efef9d2977841
Gabriel Gamez <gabrieljgamezr@gmail.com>
2024-02-04 17:52:48 -0400

    feat(products): show view and test

---
+++ test/controllers/products_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class ProductsControllerTest < ActionDispatch::IntegrationTest
+  test 'render a list of products' do
+    get products_path
+
+    assert_response :success
+    assert_select '.product', 2
+  end
+
+  test 'render a detailed product page' do
+    get product_path(products(:one))
+
+    assert_response :success
+    assert_select '.title', 'Title: PS4'
+    assert_select '.description', 'Description: Is a PS4'
+    assert_select '.price', 'Price: 400'
+  end
+end
