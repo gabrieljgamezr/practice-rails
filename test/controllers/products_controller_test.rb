cd716a2866917034f6222837cbbf9c1ba4937e0a
Gabriel Gamez <gabrieljgamezr@gmail.com>
2024-02-06 23:59:33 -0400

    feat(products): update a product

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -48,4 +48,34 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :unprocessable_entity
   end
+
+  test 'render an edit product form' do
+    get edit_product_path(products(:one))
+
+    assert_response :success
+    assert_select 'form'
+  end
+
+  test 'allow to update a product' do
+    patch product_path(products(:one)), params: {
+      product: {
+        price: 100
+      }
+    }
+
+    assert_redirected_to products_path
+    assert_equal flash[:notice], 'Successful operation'
+  end
+
+  test 'does not allow to update a product with empty fields' do
+    patch product_path(products(:one)), params: {
+      product: {
+        title: '',
+        description: 'Is a Nintendo 64',
+        price: 200
+      }
+    }
+
+    assert_response :unprocessable_entity
+  end
 end
