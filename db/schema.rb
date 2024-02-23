5b8368b74df3def0c1b47a7eb4f1e9f966a1af9d
Gabriel Gamez <gabrieljgamezr@gmail.com>
2024-02-22 22:09:51 -0400

    fix: add name category not null and translate in meta title

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_13_020553) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_23_015912) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,7 +43,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_13_020553) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
