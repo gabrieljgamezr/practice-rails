04cf73b0986ebc660b2424d0347cfbb51cfc4d29
Gabriel Gamez <gabrieljgamezr@gmail.com>
2024-02-25 23:01:35 -0400

    feat(auth): protect pages

--- test/controllers/authentication/sessions_controller_test.rb
+++ test/controllers/authentication/sessions_controller_test.rb
@@ -6,24 +6,24 @@ class Authentication::SessionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
-    get new_sesion_url
+    get new_session_url
     assert_response :success
   end
 
   test "should login an users by email" do
-    post sessions_url, params: {{
+    post sessions_url, params: {
       login: @user.email,
       password: 'testme',
-    }}
+    }
 
     assert_redirected_to products_url
   end
 
   test "should login an users by username" do
-    post sessions_url, params: {{
+    post sessions_url, params: {
       login: @user.username,
       password: 'testme',
-    }}
+    }
 
     assert_redirected_to products_url
   end
