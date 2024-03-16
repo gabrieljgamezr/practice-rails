b5e1f82cf5e6fd15444390190228e0633ebc510f
Gabriel Gamez <gabrieljgamezr@gmail.com>
2024-03-16 00:14:43 -0400

    refactor: logout and improvements

--- test/controllers/authentication/sessions_controller_test.rb
+++ test/controllers/authentication/sessions_controller_test.rb
@@ -27,4 +27,12 @@ class Authentication::SessionsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to products_url
   end
+
+  test "should logout" do
+    login
+
+    delete session_url(@user.id)
+    assert_redirected_to products_url
+    assert_equal flash[:notice], 'See you later...'
+  end
 end
